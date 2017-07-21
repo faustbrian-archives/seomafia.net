d9a52ffaf6ded55c2664a80b93d4dedeaf274c4b
Brian Faust <hello@brianfaust.de>
2017-07-21 08:18:28 +0300

    Refactor to use faustbrian/http

---
+++ src/API/Links.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * This file is part of SeoMafia.net PHP Client.
+ *
+ * (c) Brian Faust <hello@brianfaust.me>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace BrianFaust\SeoMafiaNet\API;
+
+use BrianFaust\Http\HttpResponse;
+
+class Links extends AbstractAPI
+{
+    /**
+     * @param string $url
+     * @param array  $parameters
+     *
+     * @return \BrianFaust\Http\HttpResponse
+     */
+    public function create(string $url, array $parameters = []): HttpResponse
+    {
+        return $this->client->post('write/post', compact('url') + $parameters);
+    }
+
+    /**
+     * @param int         $id
+     * @param string|null $password
+     *
+     * @return \BrianFaust\Http\HttpResponse
+     */
+    public function details(int $id, ?string $password = null): HttpResponse
+    {
+        return $this->client->post('read/post', compact('id', 'password'));
+    }
+}
